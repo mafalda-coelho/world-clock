cc4aec593907cc4e7391deff800f49f31175e7aa
mafalda-coelho <mafa.coel1@gmail.com>
2024-01-10 13:45:49 +0000

    added id to city in html and added accurate data to cities using javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDateElement = lisbonElement.querySelector(".date");
+  let lisbonTimeElement = lisbonElement.querySelector(".time");
+  let lisbonTime = moment().tz("Europe/Lisbon");
 
+  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+  lisbonTimeElement.innerHTML = lisbonTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
